4f69ed5bc0879d03bdafea244a86992e03a1220a
a201a <115426699+a201a@users.noreply.github.com>
2024-05-30 11:06:22 +0300

    Update manga.js

--- commands/manga.js
+++ commands/manga.js
@@ -1,25 +1,30 @@
-const axios = require('axios');
-
-module.exports = (bot) => {
-    bot.onText(/\/manga (.+) (\d+)/, async (msg, match) => {
-        const chatId = msg.chat.id;
-        const mangaName = match[1];
-        const chapterNumber = match[2];
+module.exports = async (bot) => {
+    try {
+        const { default: fetch } = await import('node-fetch');
         
-        try {
-            const url = `https://alifadel2-1.onrender.com/manga?name=${mangaName}&chapter=${chapterNumber}`;
-            const response = await axios.get(url);
+        bot.onText(/\/manga (.+) (\d+)/, async (msg, match) => {
+            const chatId = msg.chat.id;
+            const mangaName = match[1];
+            const chapterNumber = match[2];
             
-            // Assuming the response contains an array of image URLs
-            const images = response.data.images;
-            
-            // Send each image separately
-            images.forEach(async (imageUrl) => {
-                await bot.sendPhoto(chatId, imageUrl);
-            });
-        } catch (error) {
-            console.error('Error fetching manga images:', error);
-            bot.sendMessage(chatId, 'Failed to fetch manga images. Please try again later.');
-        }
-    });
+            try {
+                const url = `https://alifadel2-1.onrender.com/manga?name=${mangaName}&chapter=${chapterNumber}`;
+                const response = await fetch(url);
+                const responseData = await response.json();
+                
+                // Assuming the response contains an array of image URLs
+                const images = responseData.images;
+                
+                // Send each image separately
+                images.forEach(async (imageUrl) => {
+                    await bot.sendPhoto(chatId, imageUrl);
+                });
+            } catch (error) {
+                console.error('Error fetching manga images:', error);
+                bot.sendMessage(chatId, 'Failed to fetch manga images. Please try again later.');
+            }
+        });
+    } catch (error) {
+        console.error('Error importing node-fetch:', error);
+    }
 };
