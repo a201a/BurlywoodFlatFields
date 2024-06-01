4a9bbc9d8c9ac786213a059cc52ce47ea6623fcd
a201a <115426699+a201a@users.noreply.github.com>
2024-06-01 11:43:38 +0300

    Update commands

---
+++ commands/bardimage.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+module.exports = (bot) => {
+    bot.onText(/\/bardimage/, async (msg) => {
+        const chatId = msg.chat.id;
+        const replyToMessageId = msg.reply_to_message.message_id;
+        const defaultQuestion = "ما الذي في هذه الصورة؟";
+
+        let question = msg.text.split(" ").slice(1).join(" ");
+        question = question.trim() === "" ? defaultQuestion : question;
+
+        if (!msg.reply_to_message || !msg.reply_to_message.photo) {
+            bot.sendMessage(chatId, "يرجى الرد على صورة بأمر /bardimage.");
+            return;
+        }
+
+        const photoId = msg.reply_to_message.photo[msg.reply_to_message.photo.length - 1].file_id;
+        const imageUrl = await bot.getFileLink(photoId);
+
+        try {
+            const imgurResponse = await axios.post('https://api.imgur.com/3/image', {
+                image: imageUrl
+            }, {
+                headers: {
+                    'Authorization': 'Client-ID fc9369e9aea767c' // استبدل YOUR_CLIENT_ID بمعرف العميل الفعلي الخاص بك في Imgur
+                }
+            });
+
+            const imgurUrl = imgurResponse.data.data.link;
+
+            const bardResponse = await axios.post('https://bard.rizzy.eu.org/backend/conversation/gemini/image', {
+                ask: question,
+                image: imgurUrl
+            });
+
+            const content = bardResponse.data.content;
+            if (content) {
+                const message = `${content}`;
+                bot.sendMessage(chatId, message, { reply_to_message_id: replyToMessageId });
+            } else {
+                bot.sendMessage(chatId, "لم أتمكن من تحديد ما في الصورة.", { reply_to_message_id: replyToMessageId });
+            }
+        } catch (error) {
+            console.error("خطأ:", error);
+            bot.sendMessage(chatId, "حدث خطأ أثناء معالجة الصورة.");
+        }
+    });
+};
