a88399906f3cbc8a9f7d42ed398b045e47a6e69b
a201a <115426699+a201a@users.noreply.github.com>
2024-06-18 11:31:31 +0000

    update sharh

--- commands/sharh.js
+++ commands/sharh.js
@@ -9,15 +9,15 @@ module.exports = (bot) => {
             const response = await axios.get(`https://dorar-hadith-api-laq9.onrender.com/v1/site/sharh/text/${hadithId}`);
             const hadithInfo = response.data.data;
             const formattedHadithSharh = `
-                الحديث: ${hadithInfo.hadith}
-                الراوي: ${hadithInfo.rawi}
-                المحدث: ${hadithInfo.mohdith}
-                الكتاب: ${hadithInfo.book}
-                الصفحة/الرقم: ${hadithInfo.numberOrPage}
-                التصنيف: ${hadithInfo.grade}
-                شرح الحديث: ${hadithInfo.sharhMetadata.sharh}
+                *الحديث:* ${hadithInfo.hadith}
+                *الراوي:* ${hadithInfo.rawi}
+                *المحدث:* ${hadithInfo.mohdith}
+                *الكتاب:* ${hadithInfo.book}
+                *الصفحة/الرقم:* ${hadithInfo.numberOrPage}
+                *التصنيف:* ${hadithInfo.grade}
+                *شرح الحديث:* ${hadithInfo.sharhMetadata.sharh}
             `;
-            bot.sendMessage(chatId, formattedHadithSharh);
+            bot.sendMessage(chatId, formattedHadithSharh, { parse_mode: 'Markdown' });
         } catch (error) {
             console.error('Error:', error.message);
             bot.sendMessage(chatId, 'حدث خطأ أثناء جلب شرح الحديث.');
